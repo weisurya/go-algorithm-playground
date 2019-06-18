9b81c7de86d52350e29b75c2743007a01186f589
Soen, Surya Soenaryo <wei.surya@gmail.com>
2019-06-18 22:43:41 +0700

    Ch 2 - Arithmetic Progression

---
+++ ch2/tutorial.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 23
+	n := 9
+	d := 12
+
+	fmt.Println(arithmeticProgression(a, n, d))
+	fmt.Println(sumOfAP(a, n, d))
+}
+
+/*
+Arithmetic Progression (AP)
+the different between any 2 terms is constant
+
+a = first term of sequence
+d = constant difference
+n = nth term of AP
+T = output
+S = sum of AP
+*/
+func arithmeticProgression(a, n, d int) int {
+	return a + (n-1)*d
+}
+
+func sumOfAP(a, n, d int) int {
+	return n * (2*a + (n-1)*d) / 2
+}
