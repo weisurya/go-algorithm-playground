b5f1bb284598b65ae47d4cad8c22fc8eeb546a58
Soen, Surya Soenaryo <wei.surya@gmail.com>
2019-06-18 22:55:45 +0700

    Ch2 - Geometric Progression

--- ch2/tutorial.go
+++ ch2/tutorial.go
@@ -9,6 +9,8 @@ func main() {
 
 	fmt.Println(arithmeticProgression(a, n, d))
 	fmt.Println(sumOfAP(a, n, d))
+
+	fmt.Println(geometricProgression(a, n, d))
 }
 
 /*
@@ -27,4 +29,17 @@ func arithmeticProgression(a, n, d int) int {
 
 func sumOfAP(a, n, d int) int {
 	return n * (2 * arithmeticProgression(a, n, d)) / 2
+}
+
+/*
+Geometric Progression (GP)
+the ratio of any 2 terms is constant
+
+a = first term of sequence
+r = common ratio
+n = nth term of GP
+T = output of GP
+*/
+func geometricProgression(a, r, n int) int {
+	return a*r ^ (n - 1)
 }
